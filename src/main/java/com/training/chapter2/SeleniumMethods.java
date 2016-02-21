7f75c848257c5e64b1c253cbbeb41852c1041dce
Gurpreet Singh <gurpreetsingh@XEBIAINDIA.LOCAL>
2016-02-21 16:15:03 +0530

    modefying

--- src/main/java/com/training/chapter2/SeleniumMethods.java
+++ src/main/java/com/training/chapter2/SeleniumMethods.java
@@ -6,12 +6,21 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
 
 
 /**
  * Created by gurpreet on 4/1/16.
+ * If you don't mention the priority, it will take all the test cases as "priority=0"
+ * Elements Gets Cached Always Clear Cookie Before Executing the test  case
  */
 public class SeleniumMethods {
 
@@ -33,15 +42,20 @@ public class SeleniumMethods {
 
     @Test
     public void navigateForward() {
-        driver.navigate().to("");
-        driver.navigate().forward();
+        driver.navigate().to("http://gsbamrah.github.io");
+        driver.findElement(By.id("prac-form"));
         driver.navigate().back();
+        driver.navigate().forward();
+
         driver.navigate().refresh();
     }
 
-    @Test
+    @Test(priority=2)
     public void enterText() {
-        driver.findElement(By.id("")).sendKeys();
+        WebElement namefirst=driver.findElement(By.name("firstname"));
+       namefirst.sendKeys("gurpreet");
+        WebElement namelast=driver.findElement(By.name("lastname"));
+        namelast.sendKeys("singh");
     }
 
     @Test
@@ -49,9 +63,24 @@ public class SeleniumMethods {
         driver.findElement(By.id("")).clear();
     }
 
-    @Test
-    public void clickButton() {
-        driver.findElement(By.id("")).click();
+    @Test(priority=1)
+    public void clickButton()
+    {
+        driver.manage().window().maximize();
+        driver.get("http://gsbamrah.github.io");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        WebElement Element=driver.findElement(By.id("prac-form"));
+        Element.click();
+        WebElement Element2=driver.findElement(By.linkText("Forms"));
+        Element2.click();
+        String verifyurl=driver.getCurrentUrl();
+        Assert.assertTrue(verifyurl.contains("form.html"));
+        enterText();
+        selectfromDropDown();
+        playwithcheck();
+        SelectMultipleCheckbox();
+        driver.close();
+
     }
 
     @Test
@@ -69,11 +98,57 @@ public class SeleniumMethods {
 
     @Test
     public void DragAndDrop() {
-        Actions builder=new Actions(driver);
-    WebElement source=driver.findElement(By.id(""));
-    WebElement target=driver.findElement(By.id(""));
-       // DragAndDrop(source,target);
+        Actions builder = new Actions(driver);
+        WebElement source = driver.findElement(By.id(""));
+        WebElement target = driver.findElement(By.id(""));
+        // DragAndDrop(source,target);
+    }
 
+        @Test
+            public  void moveToElement() {
+            driver.get("http://gsbamrah.github.io");
+            WebElement Element = driver.findElement(By.id("prac-form"));
+            Actions builder = new Actions(driver);
+            builder.moveToElement(Element).perform();
+        }
+
+        @Test
+                public void MaximizeWindow()
+        {
+                driver.manage().window().maximize();
+            }
+
+    @Test
+    public void selectfromDropDown() {
+        WebElement element = driver.findElement(By.name("Country"));
+        Select sl = new Select(element);
+          sl.selectByValue("Australlia");
+    }
+        /*
+        List<WebElement> options=driver.findElements(By.tagName("Country"));
+
+        for(WebElement option:options)
+        {
+       option.click();
+    }
+*/
+
+    @Test
+    public void playwithcheck()
+    {
+        WebElement element =driver.findElement(By.xpath("//input[@name='vehicle1']"));
+        element.click();
 
     }
-}
+
+    @Test
+    public void SelectMultipleCheckbox()
+    {
+        List<WebElement> els = driver.findElements(By.xpath("//input[@type='checkbox']"));
+        for ( WebElement el : els ) {
+            if ( !el.isSelected() ) {
+                el.click();
+            }
+        }
+    }
+    }
